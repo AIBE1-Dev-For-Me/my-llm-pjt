e90c1908161f0234f5019c8152d5ed632ee58c45
taehyun <kimtaehyun002@gmail.com>
2025-02-10 12:53:16 +0900

    express&cors

---
+++ docs/app.js
@@ -0,0 +1,10 @@
+async function main() {
+  async function handleCC() {
+    document.querySelector("#box").textContent = await fetch(
+      "http://127.0.0.1:3000/"
+    ).text();
+  }
+  document.querySelector("#ccBtn").addEventListener("click", handleCC);
+}
+
+document.addEventListener("DOMContentLoaded", main);
